bfd671580e1686cc8eeb5742af8bc96efb76fd08
lupogryph <87079427+lupogryph@users.noreply.github.com>
2025-08-14 15:39:57 +0200

    Create BeansRecorder.java

---
+++ runtime/src/main/java/ch/lupogryph/quarkus/actuator/beans/BeansRecorder.java
@@ -0,0 +1,32 @@
+package ch.lupogryph.quarkus.actuator.beans;
+
+import static ch.lupogryph.quarkus.actuator.util.Headers.ACTUATOR_CONTENT_TYPE;
+import static io.vertx.core.http.HttpHeaders.CONTENT_TYPE;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import io.quarkus.arc.Arc;
+import io.quarkus.runtime.annotations.Recorder;
+import io.vertx.core.Handler;
+import io.vertx.ext.web.RoutingContext;
+
+@Recorder
+public class BeansRecorder {
+
+    private static final Logger log = LoggerFactory.getLogger(BeansRecorder.class);
+
+    public Handler<RoutingContext> beans() {
+        return rc -> {
+            var service = Arc.container().instance(BeansService.class).get();
+            log.debug("-> {} /beans", rc.request().method());
+            var json = service.contexts().build();
+            log.debug("<- {} /q : 200", rc.request().method());
+            log.trace(json);
+            rc.response()
+                    .putHeader(CONTENT_TYPE, ACTUATOR_CONTENT_TYPE)
+                    .end(json);
+        };
+    }
+
+}
