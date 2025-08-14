0ba3e46f21b5e5348684c69a86722a1df7ba8285
lupogryph <87079427+lupogryph@users.noreply.github.com>
2025-08-14 15:47:51 +0200

    Create Headers.java

---
+++ runtime/src/main/java/ch/lupogryph/quarkus/actuator/util/Headers.java
@@ -0,0 +1,5 @@
+package ch.lupogryph.quarkus.actuator.util;
+
+public final class Headers {
+    public static final String ACTUATOR_CONTENT_TYPE = "application/vnd.spring-boot.actuator.v3+json";
+}
