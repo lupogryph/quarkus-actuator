a5b2f75bb804d1ef2b4b1dd221902a9a34fd014c
lupogryph <87079427+lupogryph@users.noreply.github.com>
2026-01-14 13:02:06 +0100

    Refactor loggers handlers to use a map-based approach

--- runtime/src/main/java/ch/lupogryph/quarkus/actuator/loggers/LoggersRecorder.java
+++ runtime/src/main/java/ch/lupogryph/quarkus/actuator/loggers/LoggersRecorder.java
@@ -1,15 +1,15 @@
 package ch.lupogryph.quarkus.actuator.loggers;
 
-import static ch.lupogryph.quarkus.actuator.util.Headers.ACTUATOR_CONTENT_TYPE;
-import static io.vertx.core.http.HttpHeaders.CONTENT_TYPE;
+import static ch.lupogryph.quarkus.actuator.util.Handler.handler;
 import static io.vertx.core.http.HttpMethod.*;
+
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import io.quarkus.runtime.annotations.Recorder;
 import io.vertx.core.Handler;
-import io.vertx.core.json.Json;
 import io.vertx.ext.web.RoutingContext;
 
 @Recorder
@@ -18,49 +18,16 @@ public class LoggersRecorder {
     Logger log = LoggerFactory.getLogger(LoggersRecorder.class);
 
     public Handler<RoutingContext> loggers() {
-        return rc -> {
-            var method = rc.request().method();
-
-            log.debug("-> {} /loggers", method);
-
-            if (OPTIONS == method) {
-                rc.response().end();
-            } else if (GET == method) {
-                var loggers = LoggersService.getLoggers();
-                var json = Json.encode(loggers);
-                log.debug("<- GET /loggers : 200");
-                log.trace(json);
-                rc.response().putHeader(CONTENT_TYPE, ACTUATOR_CONTENT_TYPE).end(json);
-            } else {
-                rc.response().setStatusCode(405).end(); // Method not allowed
-            }
-        };
+        return handler(Map.of(
+                GET, rc -> LoggersService.getLoggers()));
     }
 
     public Handler<RoutingContext> namedLoggers() {
-        return rc -> {
-            var name = rc.pathParams().get("name");
-            var method = rc.request().method();
-
-            log.debug("-> {} /loggers/{}", method, name);
-            log.trace(rc.body().asString());
-
-            if (GET == method) {
-                var loggers = LoggersService.getLoggers(name);
-                var json = Json.encode(loggers);
-                log.debug("<- GET /loggers/{}", name);
-                log.trace(json);
-                rc.response().putHeader(CONTENT_TYPE, ACTUATOR_CONTENT_TYPE).end(json);
-            } else if (POST == method) {
-                var loggersRequest = rc.body().asPojo(LoggersRequest.class);
-                LoggersService.setLogger(name, loggersRequest);
-                log.debug("<- POST /loggers/{} : 204", name);
-                rc.response().setStatusCode(204).end();
-            } else {
-                rc.response().setStatusCode(405).end(); // Method not allowed
-            }
-
-        };
+        return handler(Map.of(
+                GET, rc -> LoggersService.getLoggers(rc.pathParams().get("name")),
+                POST, rc -> LoggersService.setLogger(
+                        rc.pathParams().get("name"),
+                        rc.body().asPojo(LoggersRequest.class))));
     }
 
 }
