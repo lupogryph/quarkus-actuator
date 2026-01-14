21053b6ad4341711fcd1423646e2d7e3ce501cd4
lupogryph <87079427+lupogryph@users.noreply.github.com>
2026-01-14 12:59:32 +0100

    Refactor ConfigsRecorder to simplify configs method
    Refactor configs method to use handler for GET requests.

--- runtime/src/main/java/ch/lupogryph/quarkus/actuator/configs/ConfigsRecorder.java
+++ runtime/src/main/java/ch/lupogryph/quarkus/actuator/configs/ConfigsRecorder.java
@@ -1,10 +1,9 @@
 package ch.lupogryph.quarkus.actuator.configs;
 
-import static ch.lupogryph.quarkus.actuator.util.Headers.ACTUATOR_CONTENT_TYPE;
-import static io.vertx.core.http.HttpHeaders.CONTENT_TYPE;
+import static ch.lupogryph.quarkus.actuator.util.Handler.handler;
+import static io.vertx.core.http.HttpMethod.GET;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.util.Map;
 
 import io.quarkus.arc.Arc;
 import io.quarkus.runtime.annotations.Recorder;
@@ -14,19 +13,9 @@ import io.vertx.ext.web.RoutingContext;
 @Recorder
 public class ConfigsRecorder {
 
-    private static final Logger log = LoggerFactory.getLogger(ConfigsRecorder.class);
-
     public Handler<RoutingContext> configs() {
-        return rc -> {
-            var service = Arc.container().instance(ConfigsService.class).get();
-            log.debug("-> {} /configprops", rc.request().method());
-            var json = service.contexts().build();
-            log.debug("<- {} /q : 200", rc.request().method());
-            log.trace(json);
-            rc.response()
-                    .putHeader(CONTENT_TYPE, ACTUATOR_CONTENT_TYPE)
-                    .end(json);
-        };
+        var service = Arc.container().instance(ConfigsService.class).get();
+        return handler(Map.of(GET, rc -> service.contexts().build()));
     }
 
 }
