7af5c9b110f672e57b2eb2eb6d5fc9580081acaa
lupogryph <87079427+lupogryph@users.noreply.github.com>
2026-01-14 13:30:28 +0100

    Add unit tests for CachesService

---
+++ runtime/src/test/java/ch/lupogryph/quarkus/actuator/caches/CachesServiceTest.java
@@ -0,0 +1,38 @@
+package ch.lupogryph.quarkus.actuator.caches;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import jakarta.inject.Inject;
+
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.cache.Cache;
+import io.quarkus.cache.CacheName;
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+public class CachesServiceTest {
+
+    private static final String CACHE_NAME = "test-cache";
+
+    @Inject
+    CachesService cachesService;
+
+    @CacheName(CACHE_NAME)
+    Cache cache;
+
+    @Test
+    void cacheManagersTest() {
+        var cacheManagers = cachesService.allCaches().build();
+        assertThat(cacheManagers).isNotBlank();
+    }
+
+    @Test
+    void cacheTest() {
+        var res = cachesService.cache(CACHE_NAME).build();
+        assertThat(res).isNotBlank().startsWith("""
+                {"target":"io.quarkus.cache.runtime.caffeine.CaffeineCacheImpl","name":"%s","cacheManager":"CacheManager_"""
+                .formatted(CACHE_NAME));
+    }
+
+}
