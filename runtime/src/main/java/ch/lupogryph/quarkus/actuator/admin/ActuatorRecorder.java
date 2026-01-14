26b22eeedfef4b84b2e7ca31b3b18c7c3f2b4152
lupogryph <87079427+lupogryph@users.noreply.github.com>
2026-01-14 12:49:46 +0100

    Refactor ActuatorRecorder to include caching and quartz

--- runtime/src/main/java/ch/lupogryph/quarkus/actuator/admin/ActuatorRecorder.java
+++ runtime/src/main/java/ch/lupogryph/quarkus/actuator/admin/ActuatorRecorder.java
@@ -1,6 +1,6 @@
 package ch.lupogryph.quarkus.actuator.admin;
 
-import static ch.lupogryph.quarkus.actuator.util.Headers.ACTUATOR_CONTENT_TYPE;
+import static com.lodh.arte.actuator.util.Headers.ACTUATOR_CONTENT_TYPE;
 import static io.vertx.core.http.HttpHeaders.CONTENT_TYPE;
 
 import java.net.UnknownHostException;
@@ -10,6 +10,7 @@ import org.slf4j.LoggerFactory;
 
 import io.quarkus.runtime.LaunchMode;
 import io.quarkus.runtime.annotations.Recorder;
+import io.quarkus.vertx.http.runtime.devmode.Json;
 import io.quarkus.vertx.http.runtime.management.ManagementConfig;
 import io.vertx.core.Handler;
 import io.vertx.ext.web.RoutingContext;
@@ -19,7 +20,10 @@ public class ActuatorRecorder {
 
     private static final Logger log = LoggerFactory.getLogger(ActuatorRecorder.class);
 
-    public Handler<RoutingContext> actuator(ActuatorConfig actuatorConfig, ManagementConfig managementConfig) {
+    String baseUrl;
+
+    public Handler<RoutingContext> actuator(ActuatorConfig actuatorConfig, ManagementConfig managementConfig, boolean caches,
+            boolean quartz) {
 
         String host;
         try {
@@ -29,126 +33,63 @@ public class ActuatorRecorder {
         }
 
         var port = managementConfig.determinePort(LaunchMode.current());
-        var baseUrl = "http://%s:%d/q".formatted(host, port);
+        this.baseUrl = "http://%s:%d/q".formatted(host, port);
+
+        var json = Json.object();
+
+        var links = Json.object();
+        links.put("self", link("self", "/actuator"));
+        links.put("beans", link("beans", "/actuator/beans"));
+        if (caches) {
+            links.put("caches-cache", link("caches-cache", "/actuator/caches/{cache}"));
+            links.put("caches", link("caches", "/actuator/caches"));
+        }
+        links.put("health", link("health", "/health"));
+        links.put("health-path", link("health-path", "/health/{*path}"));
+        links.put("info", link("info", "/info"));
+        links.put("conditions", link("conditions", "/actuator/conditions"));
+        links.put("configprops", link("configprops", "/actuator/configprops"));
+        links.put("configprops-prefix", link("configprops-prefix", "/actuator/configprops/{prefix}"));
+        links.put("env", link("env", "/actuator/env"));
+        links.put("env-toMatch", link("env-toMatch", "/actuator/env/{toMatch}"));
+        links.put("loggers", link("loggers", "/actuator/loggers"));
+        links.put("loggers-name", link("loggers-name", "/actuator/loggers/{name}"));
+        links.put("threaddump", link("threaddump", "/actuator/threaddump"));
+        links.put("heapdump", link("heapdump", "/actuator/heapdump"));
+        links.put("metrics-requiredMetricName", link("metrics-requiredMetricName", "/actuator/metrics/{requiredMetricName}"));
+        links.put("metrics", link("metrics", "/actuator/metrics"));
+        if (quartz) {
+            links.put("quartz", link("quartz", "/actuator/quartz"));
+            links.put("quartz-jobs-group-name", link("quartz-jobs-group-name", "/actuator/quartz/{jobs}/{group}/{name}"));
+            links.put("quartz-jobsOrTriggers-group",
+                    link("quartz-jobsOrTriggers-group", "/actuator/quartz/{jobsOrTriggers}/{group}"));
+            links.put("quartz-jobsOrTriggers", link("quartz-jobsOrTriggers", "/actuator/quartz/{jobsOrTriggers}"));
+            links.put("quartz-jobsOrTriggers-group-name",
+                    link("quartz-jobsOrTriggers-group-name", "/actuator/quartz/{jobsOrTriggers}/{group}/{name}"));
+        }
+        //links.put("sbom-id", link("sbom-id", "/actuator/sbom/{id}"));
+        //links.put("sbom", link("sbom", "/actuator/sbom"));
+        //links.put("scheduledtasks", link("scheduledtasks", "/actuator/scheduledtasks"));
+        //links.put("mappings", link("mappings", "/actuator/mappings"));
+
+        json.put("_links", links);
+
+        var response = json.build();
 
         return rc -> {
-            log.debug("-> {} /q", rc.request().method());
-            var json = """
-                    {
-                      "_links": {
-                        "self": {
-                          "href": "%1$s",
-                          "templated": false
-                        },
-                        "beans": {
-                          "href": "%1$s/beans",
-                          "templated": false
-                        },
-                        "caches-cache": {
-                          "href": "%1$s/caches/{cache}",
-                          "templated": true
-                        },
-                        "caches": {
-                          "href": "%1$s/caches",
-                          "templated": false
-                        },
-                        "health": {
-                          "href": "%1$s/health",
-                          "templated": false
-                        },
-                        "health-path": {
-                          "href": "%1$s/health/{*path}",
-                          "templated": true
-                        },
-                        "info": {
-                          "href": "%1$s/info",
-                          "templated": false
-                        },
-                        "conditions": {
-                          "href": "%1$s/conditions",
-                          "templated": false
-                        },
-                        "configprops": {
-                          "href": "%1$s/configprops",
-                          "templated": false
-                        },
-                        "configprops-prefix": {
-                          "href": "%1$s/configprops/{prefix}",
-                          "templated": true
-                        },
-                        "env": {
-                          "href": "%1$s/env",
-                          "templated": false
-                        },
-                        "env-toMatch": {
-                          "href": "%1$s/env/{toMatch}",
-                          "templated": true
-                        },
-                        "loggers": {
-                          "href": "%1$s/loggers",
-                          "templated": false
-                        },
-                        "loggers-name": {
-                          "href": "%1$s/loggers/{name}",
-                          "templated": true
-                        },
-                        "threaddump": {
-                          "href": "%1$s/threaddump",
-                          "templated": false
-                        },
-                        "metrics-requiredMetricName": {
-                          "href": "%1$s/metrics/{requiredMetricName}",
-                          "templated": true
-                        },
-                        "metrics": {
-                          "href": "%1$s/metrics",
-                          "templated": false
-                        },
-                        "quartz": {
-                          "href": "%1$s/quartz",
-                          "templated": false
-                        },
-                        "quartz-jobs-group-name": {
-                          "href": "%1$s/quartz/{jobs}/{group}/{name}",
-                          "templated": true
-                        },
-                        "quartz-jobsOrTriggers-group": {
-                          "href": "%1$s/quartz/{jobsOrTriggers}/{group}",
-                          "templated": true
-                        },
-                        "quartz-jobsOrTriggers": {
-                          "href": "%1$s/quartz/{jobsOrTriggers}",
-                          "templated": true
-                        },
-                        "quartz-jobsOrTriggers-group-name": {
-                          "href": "%1$s/quartz/{jobsOrTriggers}/{group}/{name}",
-                          "templated": true
-                        },
-                        "sbom-id": {
-                          "href": "%1$s/sbom/{id}",
-                          "templated": true
-                        },
-                        "sbom": {
-                          "href": "%1$s/sbom",
-                          "templated": false
-                        },
-                        "scheduledtasks": {
-                          "href": "%1$s/scheduledtasks",
-                          "templated": false
-                        },
-                        "mappings": {
-                          "href": "%1$s/mappings",
-                          "templated": false
-                        }
-                      }
-                    }
-                    """.formatted(baseUrl);
-            log.debug("<- {} /q : 200", rc.request().method());
-            log.trace(json);
+            log.debug("-> {} /q/actuator", rc.request().method());
+            log.debug("<- {} /q/actuator : 200", rc.request().method());
+            log.trace(response);
             rc.response()
                     .putHeader(CONTENT_TYPE, ACTUATOR_CONTENT_TYPE)
-                    .end(json);
+                    .end(response);
         };
+    }
+
+    Json.JsonObjectBuilder link(String name, String path) {
+        return Json.object()
+                .put("href", baseUrl + path)
+                .put("templated", path.contains("{"));
     }
 
 }
