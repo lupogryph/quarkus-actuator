94bcbbf6638ad296d03614a474fb3650cd66295e
lupogryph <87079427+lupogryph@users.noreply.github.com>
2026-01-14 13:32:28 +0100

    Add EnvServiceTest to validate environment service

---
+++ runtime/src/test/java/ch/lupogryph/quarkus/actuator/env/EnvServiceTest.java
@@ -0,0 +1,23 @@
+package ch.lupogryph.quarkus.actuator.env;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+import jakarta.inject.Inject;
+
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+public class EnvServiceTest {
+
+    @Inject
+    EnvService service;
+
+    @Test
+    void testEnv() {
+        var env = service.env().build();
+        assertThat(env).isNotBlank();
+    }
+
+}
